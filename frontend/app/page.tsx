94a2e3b4be73783e51eda406edd7b2b79e33ecd3
SanikaDhumalDevs <sanikadhumal149@gmail.com>
2025-12-31 16:56:21 +0530

    Add files via upload

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
-export const dynamic = 'force-dynamic';
 'use client';
+export const dynamic = 'force-dynamic';
+
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
